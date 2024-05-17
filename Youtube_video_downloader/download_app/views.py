05d41f1c4a9ec92ac15dc4d8300de6908d708f2f
aswanth0123 <aswanthsuresh74@gmail.com>
2024-05-17 10:43:25 +0530

    project added

---
+++ Youtube_video_downloader/download_app/views.py
@@ -0,0 +1,36 @@
+# importing all the required modules 
+from django.shortcuts import render, redirect 
+from pytube import *
+from django.contrib import messages
+  
+# defining function 
+def download1(request): 
+  
+    # checking whether request.method is post or not 
+    if request.method == 'POST': 
+        
+        # getting link from frontend 
+        # try:
+
+            link = request.POST['link'] 
+            video = YouTube(link) 
+            v_type=request.POST['type']
+            if v_type=='audio':
+                stream = video.streams.get_audio_only()
+            elif v_type=='high':
+                stream = video.streams.get_highest_resolution()
+            else:
+                stream = video.streams.get_lowest_resolution()
+            
+            op=request.FILES["folder_upload"]
+            print('op:',type(op))
+
+            stream.download(str(op)) 
+            
+        # except:
+            messages.success(request,'Not a valid link')
+        # returning HTML page 
+            return render(request, 'index.html') 
+        
+            
+    return render(request, 'index.html')
